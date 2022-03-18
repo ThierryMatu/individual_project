13b58237f829de2d46d5aa33b4ea9619fe673c3e
ThierryMatu <henocthierry@gmail.com>
2022-03-18 23:11:53 +0100

    updates image

---
+++ cst3145.js
@@ -0,0 +1,92 @@
+const lessons = [
+    {
+      id: 1,
+      subject: 'Swimming',
+      location: 'Dubai,UAE',
+      spaces: 5,
+      price: 100,
+      stars: 4,
+      image: 'images/swimming.jpg'
+    },
+    {
+      id: 2,
+      subject: 'Soccer',
+      location: 'images/Sharjah,UAE',
+      spaces: 5,
+      price: 250,
+      stars: 4,
+      image: 'images/soccer.jpg'
+    },
+    {
+      id: 3,
+      subject: 'Football',
+      location: 'Abu Dhabi,UAE',
+      spaces: 5,
+      price: 150,
+      stars: 5,
+      image: 'image/football.jpg'
+    },
+    {
+      id: 4,
+      subject: 'Basketball',
+      location: 'Ajman,UAE',
+      spaces: 5,
+      price: 200,
+      stars: 4,
+      image: 'basketball.jpg'
+    },
+    {
+      id: 5,
+      subject: 'Martial Art',
+      location: 'Al Ain, UAE',
+      spaces: 5,
+      price: 100,
+      stars: 3,
+      image: 'martial art.jpg'
+    },
+    {
+      id: 6,
+      subject: 'Gaming',
+      location: 'Dubai,UAE',
+      spaces: 5,
+      price: 150,
+      stars: 2,
+      image: 'Gaming.jpg'
+    },
+    {
+      id: 7,
+      subject: 'Music',
+      location: 'Kalba,UAE',
+      spaces: 5,
+      price: 100,
+      stars: 3,
+      image: 'music.jpg'
+    },
+    {
+      id: 8,
+      subject: 'Movies',
+      location: ' Sharjah,UAE',
+      spaces: 5,
+      price: 50,
+      stars: 4,
+      image: 'movies.jpg'
+    },
+    {
+      id: 9,
+      subject: 'Dancing',
+      location: 'Fujairah,UAE',
+      spaces: 5,
+      price: 150,
+      stars: 5,
+      image: 'dancing.jpg'
+    },
+    {
+      id: 10,
+      subject: 'Volley Ball',
+      location: 'Al Ain,UAE',
+      spaces: 5,
+      price: 100,
+      stars: 3,
+      image: 'volleyball.jpg'
+    }
+  ]
